ebd9a9af464597c5648a2ee31cc0596c008afa89
vinay bhat <bhatvinay74@gmail.com>
2025-10-29 21:07:40 +0530

    fix the sidebar scroll issue

--- apps/analytics/src/components/ui/get-urls.tsx
+++ apps/analytics/src/components/ui/get-urls.tsx
@@ -20,7 +20,7 @@ export default function GetUrls({data}:{data:custormdata}) {
   if((data?.totalStats?.length==0)) return <></>
   return (
     <div className=" w-full flex-1 flex-col items-center h-auto overflow-y-hidden  space-y-2 p-3 " >
-      <div className=' w-full sm:w-3/5 mx-auto flex h-auto items-center'>
+      <div className=' w-full sm:w-3/5 mx-auto flex flex-col h-auto items-center'>
 
       {data?.totalStats?.map((each:each)=>{
         return (
